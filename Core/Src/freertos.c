b645b4f8935e916ea5b1313de27f6cf1e51b347f
Turing <ay52159wyh@163.com>
2025-04-09 18:20:05 +0800

    tt

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -28,6 +28,7 @@
 #include "pid.h"
 #include "vofa.h"
 #include "mpu6050.h"
+#include "inv_mpu.h"
 #include "tim.h"
 #include "oled.h"
 /* USER CODE END Includes */
@@ -59,7 +60,6 @@ float Roll,Pitch,Yaw;
 osThreadId DebugTaskHandle;
 osThreadId MotorCtrlTaskHandle;
 osThreadId RPMGetTaskHandle;
-osThreadId Vofa_TaskHandle;
 osMutexId Encoder_MutexHandle;
 osMutexId Ora_MutexHandle;
 
@@ -71,7 +71,6 @@ osMutexId Ora_MutexHandle;
 void StartDebug(void const * argument);
 void StartMotorCtrl(void const * argument);
 void StartRPMGet(void const * argument);
-void StartVofa(void const * argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -127,7 +126,7 @@ void MX_FREERTOS_Init(void) {
 
   /* Create the thread(s) */
   /* definition and creation of DebugTask */
-  osThreadDef(DebugTask, StartDebug, osPriorityNormal, 0, 128);
+  osThreadDef(DebugTask, StartDebug, osPriorityNormal, 0, 256);
   DebugTaskHandle = osThreadCreate(osThread(DebugTask), NULL);
 
   /* definition and creation of MotorCtrlTask */
@@ -137,10 +136,6 @@ void MX_FREERTOS_Init(void) {
   /* definition and creation of RPMGetTask */
   osThreadDef(RPMGetTask, StartRPMGet, osPriorityNormal, 0, 128);
   RPMGetTaskHandle = osThreadCreate(osThread(RPMGetTask), NULL);
-
-  /* definition and creation of Vofa_Task */
-  osThreadDef(Vofa_Task, StartVofa, osPriorityNormal, 0, 128);
-  Vofa_TaskHandle = osThreadCreate(osThread(Vofa_Task), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -160,16 +155,27 @@ void StartDebug(void const * argument)
 {
   /* USER CODE BEGIN StartDebug */
   float pitch, roll, yaw;
+//	while(mpu_dmp_init()){
+//		HAL_Delay(200);
+//	}
   /* Infinite loop */
   for(;;)
   {
-	  time1 = xTaskGetTickCount();
+	  //time1 = xTaskGetTickCount();
     //xSemaphoreTake(Ora_MutexHandle,portMAX_DELAY);
-    MPU6050_DMP_Get_Date(&Pitch, &Roll, &Yaw);
-    pitch = -Pitch;
-    roll = Roll;  
-    yaw = Yaw;
-	  time2 = xTaskGetTickCount()-time1;
+    if(MPU6050_DMP_Get_Date(&Pitch, &Roll, &Yaw)==0){
+		pitch = -Pitch;
+		roll = Roll;  
+		yaw = Yaw;
+	}
+	  
+//	if(mpu_dmp_get_data(&Pitch,&Roll,&Yaw)==0){
+//		pitch = Pitch;
+//		roll = Roll;  
+//		yaw = Yaw;
+//	}
+//    
+	  //time2 = xTaskGetTickCount()-time1;
     //xSemaphoreGive(Ora_MutexHandle);
 //    OLED_ShowSignedNum(1,1,pitch,5); 
 //    OLED_ShowSignedNum(2,1,roll,5);
@@ -198,7 +204,7 @@ void StartMotorCtrl(void const * argument)
 	float pitch,roll,yaw;
 	PID_parameter_init(&L_pid_Speed,30,4,0.5,10000,5000,0);
 	PID_parameter_init(&R_pid_Speed,30,4,0.5,10000,5000,0);
-  PID_parameter_init(&Pid_Angle,5,0,10,800,20,0);
+  PID_parameter_init(&Pid_Angle,20,0,131.4,800,20,0);
   
   Target_RPM = 0;
 	TickType_t preTime = xTaskGetTickCount();
@@ -234,7 +240,7 @@ void StartMotorCtrl(void const * argument)
       HAL_GPIO_WritePin(GPIOB,GPIO_PIN_4,GPIO_PIN_SET);
 	  r_output = -R_pid_Speed.output;
     }
-    if(L_pid_Speed.output>0){
+     if(L_pid_Speed.output>0){
       HAL_GPIO_WritePin(GPIOA,GPIO_PIN_10,GPIO_PIN_SET);
       HAL_GPIO_WritePin(GPIOA,GPIO_PIN_11,GPIO_PIN_RESET);
 	  l_output = L_pid_Speed.output;
@@ -297,34 +303,6 @@ void StartRPMGet(void const * argument)
   }
   
   /* USER CODE END StartRPMGet */
-}
-
-/* USER CODE BEGIN Header_StartVofa */
-/**
-* @brief Function implementing the Vofa_Task thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_StartVofa */
-void StartVofa(void const * argument)
-{
-  /* USER CODE BEGIN StartVofa */
-  /* Infinite loop */
-  for(;;)
-  {	
-//	  xSemaphoreTake(Encoder_MutexHandle,portMAX_DELAY);
-//	  Vofa_SendFloat(R_pid.output/65536);
-//	  
-//	  
-//	  
-//	  Vofa_SendFloat(kp.Pid_Data);
-//	  Vofa_SendFloat(ki.Pid_Data);
-//	  Vofa_SendFloat(kd.Pid_Data);
-//	  
-//	  xSemaphoreGive(Encoder_MutexHandle);
-      osDelay(1);
-  }
-  /* USER CODE END StartVofa */
 }
 
 /* Private application code --------------------------------------------------*/
